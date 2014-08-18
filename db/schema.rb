2f23d8273165540ec306c6a068d2631e679579ef
caedes <romain.laurent@wopata.com>
2014-08-18 11:43:17 +0200

    Add Exploit

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140818090520) do
+ActiveRecord::Schema.define(version: 20140818092707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "exploit_types", force: true do |t|
     t.string "name"
+  end
+
+  create_table "exploits", force: true do |t|
+    t.date     "held_at"
+    t.string   "title"
+    t.integer  "exploit_type_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
