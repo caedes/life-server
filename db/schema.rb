ea6aff12df3d1f59768db4353ffc661af81a835a
caedes <romain.laurent@wopata.com>
2014-08-18 11:18:32 +0200

    Add ExploitType

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140816122355) do
+ActiveRecord::Schema.define(version: 20140818090520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "exploit_types", force: true do |t|
+    t.string "name"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,6 @@
 user = User.find_or_create_by email: 'changeme@gmail.com'
 user.update_attributes password: 'changeme', password_confirmation: 'changeme'
+
+['heart', 'thumbs-o-up', 'child', 'calendar'].each do |exploit_type_name|
+  ExploitType.find_or_create_by name: exploit_type_name
+end
